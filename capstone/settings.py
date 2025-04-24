49a78f20ce97f801b538ffe673841827cef30f47
VinayaksharmaHJRFVHJ <rajasharma5210@gmail.com>
2025-04-24 23:09:01 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
